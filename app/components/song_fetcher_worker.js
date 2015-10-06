12db2017c066e788d304cae657e7a4d463eb9ad4
slothy <slothyrulez@gmail.com>
2015-10-06 15:24:43 +0200

    Dependencies & a little test

--- app/components/song_fetcher_worker.js
+++ app/components/song_fetcher_worker.js
@@ -1,7 +1,10 @@
 // WROKER FETCHING SONGS
 
+import { SockJS } from "sockjs-client";
+
 self.addEventListener("message", function(eve){
   let data = eve.data;
+  sjs = new SockJS();
   console.log("ON W DATA", data);
   self.postMessage('WORKER STARTED: ' + data);
 });
