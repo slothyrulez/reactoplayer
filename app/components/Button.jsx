2bb34243153f0b3ac0ea9feb14054200ef06fa14
slothy <slothyrulez@gmail.com>
2015-10-24 20:48:21 +0200

    Better handling toggle

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -4,12 +4,6 @@ export class Button extends  React.Component {
   static propTypes = {
     onButtonClick: PropTypes.func.isRequired
   }
-  //   onAddClick: PropTypes.func.isRequired
-  //   autoPlay: React.PropTypes.bool.isRequired,
-  //   maxLoops: React.PropTypes.number.isRequired,
-  //   posterFrameSrc: React.PropTypes.string.isRequired,
-  //   videoSrc: React.PropTypes.string.isRequired,
-  // }
   constructor(props) {
     super(props);
     this.button_name = "";
@@ -33,13 +27,8 @@ export class Button extends  React.Component {
 export class ToggleButton extends  React.Component {
   static propTypes = {
     onButtonClick: PropTypes.func.isRequired
+    playing: React.PropTypes.bool.isRequired,
   }
-  //   onAddClick: PropTypes.func.isRequired
-  //   autoPlay: React.PropTypes.bool.isRequired,
-  //   maxLoops: React.PropTypes.number.isRequired,
-  //   posterFrameSrc: React.PropTypes.string.isRequired,
-  //   videoSrc: React.PropTypes.string.isRequired,
-  // }
   constructor(props) {
     super(props);
     this._toggle = 0;
@@ -47,17 +36,9 @@ export class ToggleButton extends  React.Component {
     this.icon_names = [];
     this.button_actions = [];
     this.className = "button";
-    this._toggle_state = this._toggle_state.bind(this);
     this.button_action = this.button_action.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.icon_name = this.icon_name.bind(this);
-  }
-  _toggle_state() {
-    if (this._toggle) {
-      this._toggle = 0;
-    } else {
-      this._toggle = 1;
-    }
   }
   icon_name() {
     return this.icon_names[this._toggle];
@@ -67,10 +48,12 @@ export class ToggleButton extends  React.Component {
   }
   componentWillMount() {
     // Internal status _toggle is related to external state playing
-    if (this.props.playing) {
-      this._toggle = 1;
-    } else {
-      this._toggle = 0;
+    this._toggle = (this.props.playing)? 1: 0;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    // Internal status _toggle is related to external state playing
+    if (this.props.playing !== nextProps.playing){
+      this._toggle = (nextProps.playing)? 1: 0;
     }
   }
   render() {
@@ -81,8 +64,7 @@ export class ToggleButton extends  React.Component {
     )
   }
   handleClick(e) {
-    // Toggle internal status 
-    this._toggle_state();
+    // Toggle internal status
     this.props.onButtonClick(e, this.button_action());
   }
 }
